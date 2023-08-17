9221b97a7f6064e4c0954249156279e4b1e909ca
Mark <Loop>
2023-08-17 03:11:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число больше второго.");
+    Console.WriteLine("Второе число меньше первого.");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("Второе число больше первого.");
+    Console.WriteLine("Первое число меньше второго.");
+}
+else
+{
+    Console.WriteLine("Числа равны.");
+}
+
---
+++ ConsoleApp1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число не является чётным");
+}
